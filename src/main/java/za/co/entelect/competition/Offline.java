07d879061be2b48935396023f0c2a602c26dbc7c
Lucas Dreyer <luc4sdreyer@gmail.com>
2013-07-05 08:50:48 +0200

    Offline code update

--- src/main/java/za/co/entelect/competition/Offline.java
+++ src/main/java/za/co/entelect/competition/Offline.java
@@ -1,5 +1,22 @@
 package za.co.entelect.competition;
 
+/**
+ * map types: 
+ * 		Tank1A	0	A
+ * 		Tank1B	1	B
+ * 		Base1	2	C
+ * 		Tank2A	3	X
+ * 		Tank2B	4	Y
+ * 		Base2	5	Z
+ * 		Bullet	6	*
+ * 		Wall	7	W
+ * 		Empty	8	
+ * 		
+ * @author luc4s_000
+ *
+ */
+
+
 public class Offline {
 	public static void main(String[] args) {
 		

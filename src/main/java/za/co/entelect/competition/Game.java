8090b144da55ebc33dc537740a426432450864c2
Lucas Dreyer <luc4sdreyer@gmail.com>
2013-07-24 00:26:01 +0200

    Game can now load from GameState.toString(). Also moved server-only code to GameEx.java

--- src/main/java/za/co/entelect/competition/Game.java
+++ src/main/java/za/co/entelect/competition/Game.java
@@ -6,22 +6,8 @@ import java.io.FileNotFoundException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Scanner;
 
-import za.co.entelect.challenge.Action;
-import za.co.entelect.challenge.Board;
-import za.co.entelect.challenge.Direction;
-import za.co.entelect.challenge.SetAction;
-import za.co.entelect.challenge.State;
-import za.co.entelect.challenge.StateArray;
-//import za.co.entelect.challenge.Board;
-//import za.co.entelect.challenge.Direction;
-//import za.co.entelect.challenge.Events;
-//import za.co.entelect.challenge.Player;
-//import za.co.entelect.challenge.State;
-//import za.co.entelect.challenge.StateArray;
-//import za.co.entelect.challenge.Unit;
 import za.co.entelect.competition.bots.Bot;
 
 public class Game {
@@ -29,8 +15,7 @@ public class Game {
 	private Bot player1;
 	private Bot player2;
 	private GameState gameState;
-	private za.co.entelect.challenge.Game eGame;
-	private Board eBoard;
+	protected za.co.entelect.challenge.Game[] eGame;
 	private boolean generateEGame;
 	private Result result;
 	
@@ -144,8 +129,7 @@ public class Game {
 			this.eGame = null;
 			this.eGame = getEGame();
 			
-			this.eBoard = null;
-			this.eBoard = getEBoard();
+			this.resetEBoard();
 		}
 		
 		boolean isActive = this.gameState.isActive();
@@ -155,6 +139,9 @@ public class Game {
 		return isActive;
 	}
 	
+	public void resetEBoard() {
+	}
+
 	public void generateResult(Result result) {		
 		result.score.name[0] = this.getPlayer1().getName();
 		result.score.name[1] = this.getPlayer2().getName();
@@ -207,13 +194,29 @@ public class Game {
 				} else if (file.get(y).charAt(x) == '#') {
 					map[y][x] = 1;
 				} else if (file.get(y).charAt(x) == 'A') {
-					tanks[0] = new Tank(new Point(x,y), 2, null);
+					if (tanks[0] == null) {
+						tanks[0] = new Tank(new Point(x,y), 2, null);
+					} else {
+						map[y][x] = 0;
+					}
 				} else if (file.get(y).charAt(x) == 'B') {
-					tanks[1] = new Tank(new Point(x,y), 2, null);
+					if (tanks[1] == null) {
+						tanks[1] = new Tank(new Point(x,y), 2, null);
+					} else {
+						map[y][x] = 0;
+					}
 				} else if (file.get(y).charAt(x) == 'X') {
-					tanks[2] = new Tank(new Point(x,y), 2, null);
+					if (tanks[2] == null) {
+						tanks[2] = new Tank(new Point(x,y), 2, null);
+					} else {
+						map[y][x] = 0;
+					}
 				} else if (file.get(y).charAt(x) == 'Y') {
-					tanks[3] = new Tank(new Point(x,y), 2, null);
+					if (tanks[3] == null) {
+						tanks[3] = new Tank(new Point(x,y), 2, null);
+					} else {
+						map[y][x] = 0;
+					}
 				} else if (file.get(y).charAt(x) == 'C') {
 					bases[0] = new Base(new Point(x,y), 2);
 					map[y][x] = Unit.BASE1;
@@ -229,6 +232,10 @@ public class Game {
 		}
 		for (int i = 0; i < tanks.length; i++) {
 			Tank t = tanks[i];
+			if (tanks[i] == null) {
+				tanks[i] = new Tank(new Point(0,0), 2, null, false);
+				continue;
+			}
 			for (int y2 = 0; y2 < GameState.tankSize; y2++) {
 				for (int x2 = 0; x2 < GameState.tankSize; x2++) {
 					map[t.getPosition().y+y2][t.getPosition().x+x2] = Unit.TANK1A+i;
@@ -241,170 +248,7 @@ public class Game {
 		return newGame;
 	}
 	
-	public za.co.entelect.challenge.Game getEGame() {
-		
-		if (this.eGame == null) {
-			this.eGame = new za.co.entelect.challenge.Game();
-			
-			//za.co.entelect.challenge.Player[] players = new za.co.entelect.challenge.Player[2];
-			
-			
-			for (int i = 0; i < 2; i++) {
-				za.co.entelect.challenge.Player ePlayer = new za.co.entelect.challenge.Player();
-				
-				Base xBase = getGameState().getBases()[i];
-				if (xBase.isAlive()) {
-					za.co.entelect.challenge.Base eBase = new za.co.entelect.challenge.Base();
-					eBase.setX(xBase.getPosition().x);
-					eBase.setY(xBase.getPosition().y);				
-					ePlayer.setBase(eBase);
-				}
-				
-				for (int tankIdx = 0; tankIdx < getGameState().getTanks().length; tankIdx++) {
-					Tank xTank = getGameState().getTanks()[tankIdx];
-					if (xTank.isAlive() && tankIdx/2 == i) {
-						za.co.entelect.challenge.Unit eUnit = new za.co.entelect.challenge.Unit();
-						eUnit.setId(tankIdx);
-						eUnit.setX(xTank.getPosition().x);
-						eUnit.setY(xTank.getPosition().y);						
-						
-						Direction eDirection = null;
-						switch (xTank.getRotation()) {
-							case GameAction.NORTH:	eDirection = Direction.UP;		break;
-							case GameAction.EAST:	eDirection = Direction.RIGHT;	break;
-							case GameAction.SOUTH:	eDirection = Direction.DOWN;	break;
-							case GameAction.WEST:	eDirection = Direction.LEFT;	break;
-						}
-						eUnit.setDirection(eDirection);
-						
-						Action eAction = Action.NONE;
-						if (xTank.getNextAction() != null) {
-							switch (xTank.getNextAction().type) {
-								case GameAction.MOVE:
-									switch (xTank.getRotation()) {
-										case GameAction.NORTH:	eAction = Action.UP;	break;
-										case GameAction.EAST:	eAction = Action.RIGHT;	break;
-										case GameAction.SOUTH:	eAction = Action.DOWN;	break;
-										case GameAction.WEST:	eAction = Action.LEFT;	break;
-									}
-									break;
-								case GameAction.FIRE:	eAction = Action.FIRE;	break;
-								case GameAction.NONE:	eAction = Action.NONE;	break;
-							}
-						}
-						eUnit.setAction(eAction);
-						
-						ePlayer.addUnits(eUnit);
-					}
-				}
-				
-				for (int bulletIdx = 0; bulletIdx < getGameState().getBullets().length; bulletIdx++) {
-					Bullet xBullet = getGameState().getBullets()[bulletIdx];
-					if (xBullet.isAlive() && bulletIdx/2 == i) {
-						za.co.entelect.challenge.Bullet eBullet = new za.co.entelect.challenge.Bullet();
-						eBullet.setId(bulletIdx);
-						eBullet.setX(xBullet.getPosition().x);
-						eBullet.setY(xBullet.getPosition().y);						
-						
-						Direction eDirection = null;
-						switch (xBullet.getRotation()) {
-							case GameAction.NORTH:	eDirection = Direction.UP;		break;
-							case GameAction.EAST:	eDirection = Direction.RIGHT;	break;
-							case GameAction.SOUTH:	eDirection = Direction.DOWN;	break;
-							case GameAction.WEST:	eDirection = Direction.LEFT;	break;
-						}
-						eBullet.setDirection(eDirection);
-						
-						ePlayer.addBullets(eBullet);
-					}
-				}
-				this.eGame.addPlayers(ePlayer);
-			}		
-									
-			this.eGame.setCurrentTick(getGameState().getTickCount());
-			System.out.println("this.eGame.getCurrentTick(): " + this.eGame.getCurrentTick());
-			
-			za.co.entelect.challenge.Events events = new za.co.entelect.challenge.Events();
-			
-			//BlockEvent b = new BlockEvent();
-			//b.setNewState(State.EMPTY);			
-			//za.co.entelect.challenge.Point ePoint = new za.co.entelect.challenge.Point();
-			//ePoint.setX(0);
-			//ePoint.setY(0);
-			//b.setPoint(ePoint);
-			//events.addBlockEvents(b);		
-			
-			//UnitEvent u = new UnitEvent();
-			//events.addUnitEvents(u);			
-			this.eGame.setEvents(events);
-			
-			this.getEBoard();
-			
-			Calendar c = Calendar.getInstance();
-			c.add(Calendar.SECOND, 3);	
-			this.eGame.setNextTickTime(c);			
-		}
-		
-		return this.eGame;
-	}
-	
-	public Board getEBoard() {
-		if (this.eBoard == null) {
-			this.eBoard = new Board();
-			
-			int[][] map = this.getGameState().getMap();
-			for (int y = 0; y < map.length; y++) {
-				StateArray eStateArray = new StateArray();
-				for (int x = 0; x < map[0].length; x++) {
-					State eState = null;
-					switch (map[y][x]) {
-						case Unit.WALL:		eState = State.FULL;	break;
-						default:			eState = State.EMPTY;	break;
-					}
-					eStateArray.addItem(eState);
-				}
-				this.eBoard.addStates(eStateArray);
-			}
-		}
-		return this.eBoard;
-	}
-	public void setAction(SetAction action, int playerIdx) {
-//		ArrayList<Tank> tanks = new ArrayList<Tank>();
-//		for (int tankIdx = 0; tankIdx < this.getGameState().getTanks().length; tankIdx++) {
-//			Tank xTank = getGameState().getTanks()[tankIdx];
-//			if (xTank.isAlive() && tankIdx/2 == playerIdx) {
-//				tanks.add(xTank);
-//			}
-//		}
-		
-		
-		GameAction xGameAction = null;
-		
-		switch (action.getArg1().getValue()) {
-			case "UP":		xGameAction = new GameAction(GameAction.MOVE, GameAction.NORTH);	break;
-			case "RIGHT":	xGameAction = new GameAction(GameAction.MOVE, GameAction.EAST);		break;
-			case "DOWN":	xGameAction = new GameAction(GameAction.MOVE, GameAction.SOUTH);	break;
-			case "LEFT":	xGameAction = new GameAction(GameAction.MOVE, GameAction.WEST);		break;
-			case "FIRE":	xGameAction = new GameAction(GameAction.FIRE, GameAction.NORTH);	break;
-			case "NONE":	xGameAction = new GameAction(GameAction.NONE, GameAction.NORTH);	break;
-		}
-		
-		//tanks.get(action.getArg0()).setNextAction(xGameAction);
-		if (playerIdx == 0) {
-			GameAction[] gameAction = this.getPlayer1().getGameActions();
-			if (gameAction == null) {
-				gameAction = new GameAction[2];
-				this.getPlayer1().setGameActions(gameAction);
-			}
-			gameAction[action.getArg0()] = xGameAction;
-		} else if (playerIdx == 1) {
-			GameAction[] gameAction = this.getPlayer2().getGameActions();
-			if (gameAction == null) {
-				gameAction = new GameAction[2];
-				this.getPlayer2().setGameActions(gameAction);
-			}
-			gameAction[action.getArg0()] = xGameAction;
-		}
-		eGame.getPlayers()[playerIdx].getUnits()[action.getArg0()].setAction(action.getArg1());
+	public za.co.entelect.challenge.Game[] getEGame() {
+		return null;
 	}
 }
